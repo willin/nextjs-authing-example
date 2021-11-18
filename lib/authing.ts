1a74aee0b065a2f0b4582848d7d3fa9b090e2a23
Willin Wang <willin@willin.org>
2021-11-18 08:48:27 +0800

    chore: :wrench: update vercel config

--- lib/authing.ts
+++ lib/authing.ts
@@ -3,7 +3,7 @@ import { AuthingSSO } from '@authing/sso';
 const authing = new AuthingSSO({
   appId: '61948f51bc0cde46337a5107',
   origin: 'https://willin.authing.cn',
-  redirectUri: 'http://localhost:3000/callback'
+  redirectUri: 'https://nextjs-authing.vercel.app/callback'
 });
 
 export default authing;
