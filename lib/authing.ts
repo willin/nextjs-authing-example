258feb37e2ef3e05bca5936e6cbaff493a9fe913
Willin Wang <willin@willin.org>
2021-11-18 10:13:39 +0800

    chore: :wrench: update domain config

--- lib/authing.ts
+++ lib/authing.ts
@@ -3,7 +3,7 @@ import { AuthingSSO } from '@authing/sso';
 const authing = new AuthingSSO({
   appId: '61948f51bc0cde46337a5107',
   origin: 'https://willin.authing.cn',
-  redirectUri: 'https://authing-next.js.cool//callback'
+  redirectUri: 'https://authing-next.js.cool/callback'
 });
 
 export default authing;
